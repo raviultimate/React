7a4fa0b1086c1ec0508702b098490ec66958c756
raviultimate <59554021+raviultimate@users.noreply.github.com>
2023-01-05 15:15:50 +0530

    Parcel introduction

--- App.js
+++ App.js
@@ -2,7 +2,9 @@ const h1 = React.createElement("h1", {}, "Namaste everyone");
 
 const h2 = React.createElement("h2", {}, "Namaste everyone");
 
-const container = React.createElement("div", { id: "container" }, [h1, h2]);
+const h3 = React.createElement("h3", {}, "Hello h3 heading");
+
+const container = React.createElement("div", { id: "container" }, [h1, h2, h3]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
