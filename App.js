52cae40e7ec9847155a6fbb2a1627b30c202620d
raviultimate <59554021+raviultimate@users.noreply.github.com>
2023-01-08 19:01:35 +0530

    Start Food Villa App

--- App.js
+++ App.js
@@ -1,11 +1,74 @@
-const h1 = React.createElement("h1", {}, "Namaste everyone");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const h2 = React.createElement("h2", {}, "Namaste everyone");
+import { restaurantList } from "./constants";
 
-const h3 = React.createElement("h3", {}, "Hello h3 heading");
+const Title = () => (
+  <h1 id="title" key="h2">
+    <img
+      className="logo"
+      alt="logo"
+      src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+    />
+  </h1>
+);
 
-const container = React.createElement("div", { id: "container" }, [h1, h2, h3]);
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <Title />
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/" +
+          props.restaurant.data.cloudinaryImageId
+        }
+      />
+      <h2>{props.restaurant.data.name}</h2>
+      <h3>{props.restaurant.data.cuisines.join(", ")}</h3>
+      <h4>{props.restaurant.data.avgRating + " stars"}</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {restaurantList.map((element) => {
+        return <RestaurantCard key={element.data.id} restaurant={element} />;
+      })}
+    </div>
+  );
+};
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <Body />
+      <Footer />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<AppLayout />);
